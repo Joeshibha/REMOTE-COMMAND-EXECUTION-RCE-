ba8895417251ac87c75ab9bc3c08796ac0583fb9
Joeshibha <67230459+Joeshibha@users.noreply.github.com>
2021-11-20 18:29:59 +0530

    Create clientRCE.java

---
+++ clientRCE.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.*;
+class clientRCE
+{
+public static void main(String args[]) throws IOException
+{
+try
+{
+String str;Socket client=new Socket("127.0.0.1",6555);
+PrintStream ps=new PrintStream(client.getOutputStream());
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+System.out.println("\t\t\t\tCLIENT WINDOW\n\n\t\tEnter TheCommand:");
+str=br.readLine();
+ps.println(str);
+}
+catch(IOException e)
+{
+System.out.println("Error"+e); }
+}
+}
